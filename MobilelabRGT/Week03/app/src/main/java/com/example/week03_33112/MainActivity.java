95a9cb8f4384b0678f31ee6a5305fa9083180636
Riyan Glory Tamba <61065512+rgt11@users.noreply.github.com>
2021-02-10 12:04:33 +0700

    Commit Week 3

---
+++ MobilelabRGT/Week03/app/src/main/java/com/example/week03_33112/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.week03_33112;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+
+import java.util.LinkedList;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> mDaftarKata = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private DaftarKataAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int jumlahKata = mDaftarKata.size() ;
+                mDaftarKata.addLast("Kata " + (jumlahKata + 1) +
+                        " telah ditambahkan");
+                Objects.requireNonNull
+                        (mRecyclerView.getAdapter()).notifyItemInserted(jumlahKata);
+                mRecyclerView.smoothScrollToPosition(jumlahKata);
+            }
+        });
+        for (int i = 1; i < 21; i++) {
+            mDaftarKata.add("Kata " + i);
+            }
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mAdapter = new DaftarKataAdapter(this, mDaftarKata);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
+
